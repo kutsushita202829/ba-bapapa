7488d46d6db3a6587e187d1443c9a43790539d21
hiroki gunji <gunji@tdc.co.jp>
2023-06-08 13:08:43 +0900

    途中

--- gobblet-app/src/App.vue
+++ gobblet-app/src/App.vue
@@ -1,26 +1,57 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="app">   
+        <table border="1">
+            <tr v-for="(row) in states" :key=row>
+                <td v-for="(column) in row" :key=column>
+                    <div class="maru" v-if="column == 1">
+                        ◯
+                    </div>
+                    <div class="batsu" v-if="column == 2">
+                        x
+                    </div>
+                </td>
+            </tr>
+        </table>
+          <p class="player1" v-if="playerId == 1">
+            プレイヤー1さんの番です
+        </p>
+        <p  class="player2" v-if="playerId == 2">
+            プレイヤー2さんの番です
+        </p>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    data () {
+        return {
+            states:[
+                [-1,-1,-1],
+                [-1,-1,-1],
+                [-1,-1,-1],
+            ],
+            playerId:1
+        }
+    }
+ }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss" scoped>
+  #app{
+      width: 500px;
+      margin:200px auto;
+  }
+  .maru,
+  .player1{
+      color:red;
+  }
+  .batsu,
+  .player2{
+      color:blue;
+  }
+  td{
+      width: 50px;
+      height: 50px;
+      text-align: center;
+  }
 </style>
